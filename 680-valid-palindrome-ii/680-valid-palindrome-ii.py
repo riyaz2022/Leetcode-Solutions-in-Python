daf7f23e98f02cdb7e28d6810d840cc5449ca03c
Riyaz Shaikh <64190913+riyaz2022@users.noreply.github.com>
2022-02-23 17:42:25 +0530

    Time: 124 ms (80.22%), Space: 14.7 MB (30.46%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l,r = 0,len(s)-1
+        while l < r:
+            if s[l] != s[r]:
+                one = s[l:r] 
+                two = s[l+1:r+1]
+                return one == one[::-1] or two == two[::-1]
+            l += 1
+            r -= 1
+        return True
+                    
