7caf4a9de71eaade75361fc712aa6656be746947
Riyaz Shaikh <64190913+riyaz2022@users.noreply.github.com>
2022-03-06 12:35:42 +0530

    Time: 44 ms (68.47%), Space: 13.8 MB (98.86%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countOrders(self, n: int) -> int:
+        MOD = 1_000_000_007
+        ans = 1
+
+        for i in range(1, n + 1):
+            # Ways to arrange all pickups, 1*2*3*4*5*...*n
+            ans = ans * i
+            # Ways to arrange all deliveries, 1*3*5*...*(2n-1)
+            ans = ans * (2 * i - 1)
+            ans %= MOD
+        
+        return ans
