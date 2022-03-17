fdb5a52eefc4d33089c4f31e4d9ed4bf209beb67
Riyaz Shaikh <64190913+riyaz2022@users.noreply.github.com>
2022-03-17 18:05:49 +0530

    Time: 32 ms (86.28%), Space: 13.9 MB (30.17%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = [0] #The score of the current frame
+
+        for x in s:
+            if x == '(':
+                stack.append(0)
+            else:
+                v = stack.pop()
+                stack[-1] += max(2 * v, 1)
+
+        return stack.pop()
